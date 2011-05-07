8b6e97872ff7191bdf4a406b094f2ebdccd0222e
cmhill <themagnum@gmail.com>
2011-05-07 21:13:31 +0000

    Fixed PresenceVector test to work with calbach's changes.

--- javatests/cbcb/kmulus/util/PresenceVectorTest.java
+++ javatests/cbcb/kmulus/util/PresenceVectorTest.java
@@ -8,18 +8,17 @@ import junit.framework.TestCase;
 /** Tests for {@link PresenceVector}. */
 public class PresenceVectorTest extends TestCase {
 	protected int kmerLength = 2;
-	protected int alphabetSize = 3;
+	protected char[] residues = {'A', 'B', 'C'};
 	
 	/** Test for {@link PresenceVector#PresenceVector(int, char[])} */
 	public void testPresenceVector() {
-		PresenceVector pv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector pv = new PresenceVector(kmerLength, residues.length);
 		assertTrue(pv != null);
 	}
 	
-	
 	/** Test for {@link PresenceVector#setKmer(int)} */
 	public void testSetKmer() {
-		PresenceVector pv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector pv = new PresenceVector(kmerLength, residues.length);
 	
 		pv.setKmer(0);
 		assertTrue(pv.containsKmer(0));
@@ -33,20 +32,20 @@ public class PresenceVectorTest extends TestCase {
 	public void testSetKmerBiology() {
 		PresenceVector pv = new PresenceVector(kmerLength, Biology.AMINO_ACIDS.length);
 	
-		pv.setKmer(Biology.getCompressedHash("AA"));
-		assertTrue(pv.containsKmer(Biology.getCompressedHash("AA")));
-		assertFalse(pv.containsKmer(Biology.getCompressedHash("AV")));
+		pv.setKmer(Biology.getAAKmerHash("AA"));
+		assertTrue(pv.containsKmer(Biology.getAAKmerHash("AA")));
+		assertFalse(pv.containsKmer(Biology.getAAKmerHash("AV")));
 		
-		pv.setKmer(Biology.getCompressedHash("VV"));
-		pv.setKmer(Biology.getCompressedHash("AA"), false);
+		pv.setKmer(Biology.getAAKmerHash("VV"));
+		pv.setKmer(Biology.getAAKmerHash("AA"), false);
 
-		assertTrue(pv.containsKmer(Biology.getCompressedHash("VV")));
-		assertFalse(pv.containsKmer(Biology.getCompressedHash("AA")));
+		assertTrue(pv.containsKmer(Biology.getAAKmerHash("VV")));
+		assertFalse(pv.containsKmer(Biology.getAAKmerHash("AA")));
 	}
 	
 	/** Test for {@link PresenceVector#PresenceVector(PresenceVector)} */
 	public void testCopyConstructor() {
-		PresenceVector pv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector pv = new PresenceVector(kmerLength, residues.length);
 		pv.setKmer(0); pv.setKmer(1); pv.setKmer(4);
 		
 		PresenceVector otherPv = new PresenceVector(pv);
@@ -58,10 +57,10 @@ public class PresenceVectorTest extends TestCase {
 	
 	/** Test for {@link PresenceVector#intersect(PresenceVector)} */
 	public void testIntersect() {
-		PresenceVector pv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector pv = new PresenceVector(kmerLength, residues.length);
 		pv.setKmer(2); pv.setKmer(1); pv.setKmer(4);
 		
-		PresenceVector otherPv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector otherPv = new PresenceVector(kmerLength, residues.length);
 		otherPv.setKmer(3); otherPv.setKmer(5); otherPv.setKmer(4);
 		
 		PresenceVector intersectPv = otherPv.intersect(pv);
@@ -71,44 +70,22 @@ public class PresenceVectorTest extends TestCase {
 	
 	/** Test for {@link PresenceVector#intersectEquals(PresenceVector)} */
 	public void testIntersectEquals() {
-		PresenceVector pv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector pv = new PresenceVector(kmerLength, residues.length);
 		pv.setKmer(2); pv.setKmer(1); pv.setKmer(4);
 		
-		PresenceVector otherPv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector otherPv = new PresenceVector(kmerLength, residues.length);
 		otherPv.setKmer(3); otherPv.setKmer(5); otherPv.setKmer(4);
 		
 		PresenceVector intersectPv = otherPv.intersectEquals(pv);
 		assertTrue(intersectPv.containsKmer(4) && !intersectPv.containsKmer(3));
 	}
 	
-	/** Test for {@link PresenceVector#intersectionCount(PresenceVector)} */
-	public void testIntersectCount() {
-		PresenceVector pv = new PresenceVector(kmerLength, 20);
-		pv.setKmer(10); pv.setKmer(69); pv.setKmer(100);
-		
-		PresenceVector otherPv = new PresenceVector(kmerLength, 20);
-		otherPv.setKmer(10); otherPv.setKmer(100); otherPv.setKmer(101);
-		assertEquals(2, pv.intersectionCount(otherPv));
-	}
-	
-	/** Test for {@link PresenceVector#countBits(int)}. */
-	public void testCountBits() {
-		PresenceVector vector = new PresenceVector();
-		assertEquals(0, vector.countBits(0x00));
-		assertEquals(1, vector.countBits(0x01));
-		assertEquals(2, vector.countBits(0x05));
-		assertEquals(4, vector.countBits(0x0F));
-		assertEquals(4, vector.countBits(0xF0));
-		assertEquals(1, vector.countBits(0x80));
-		assertEquals(8, vector.countBits(0xFF));
-	}
-	
 	/** Test for {@link PresenceVector#union(PresenceVector)} */
 	public void testUnion() {
-		PresenceVector pv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector pv = new PresenceVector(kmerLength, residues.length);
 		pv.setKmer(2); pv.setKmer(1);
 		
-		PresenceVector otherPv = new PresenceVector(kmerLength, alphabetSize);
+		PresenceVector otherPv = new PresenceVector(kmerLength, residues.length);
 		otherPv.setKmer(3); otherPv.setKmer(2);
 		
 		PresenceVector intersectPv = otherPv.union(pv);
