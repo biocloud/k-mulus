52c515c557c6d6143df416d7587bd988e36259f7
calbach <calbach88@gmail.com>
2011-05-08 01:32:03 +0000

    Retrofitted BlastMapper with PresenceVector code.

--- java/cbcb/kmulus/blast/BlastMapper.java
+++ java/cbcb/kmulus/blast/BlastMapper.java
@@ -1,26 +1,21 @@
 package cbcb.kmulus.blast;
 
-import cbcb.kmulus.util.Biology;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.log4j.Logger;
+
+import cbcb.kmulus.util.Biology;
+import cbcb.kmulus.util.PresenceVector;
 
 /**
  * Mapper recieves a chunk of input simple fasta sequences. For each k-mer seen in
@@ -34,20 +29,18 @@ public class BlastMapper extends Mapper<LongWritable, Text, LongWritable, Text> 
 
 	private int alphabetSize;
 	private int kmerLength;
-	private int numberOfBytes;
 	private int numCenters;
 	
-	// K-mer presence vector.
-	private byte[][] clusterKmerVectors;
-
-	/**
-	 * Allocate space for the cluster center vectors.
-	 */
+	/** The {@link PresenceVector} for each given cluster center. */
+	private PresenceVector[] clusterCenters;
+ 
+	/** Allocate space for the cluster center vectors. */
 	private void initializeCenters() {
-		if (clusterKmerVectors != null)
-			clusterKmerVectors = null;
+		if (clusterCenters != null) {
+			clusterCenters = null;
+		}
 
-		clusterKmerVectors = new byte[numCenters][numberOfBytes];
+		clusterCenters = new PresenceVector[numCenters];
 	}
 
 	/**
@@ -73,31 +66,27 @@ public class BlastMapper extends Mapper<LongWritable, Text, LongWritable, Text> 
 			if (srcFileStatus.isDir())
 				continue;
 
-			SequenceFile.Reader reader = new SequenceFile.Reader(fs,
-					srcFileStatus.getPath(), conf);
+			SequenceFile.Reader reader = new SequenceFile.Reader(fs,srcFileStatus.getPath(), conf);
 
 			LongWritable key = new LongWritable();
-			BytesWritable value = new BytesWritable();
+			PresenceVector value = new PresenceVector();
 
 			while (reader.next(key, value) != false) {
-				if (key.toString().equals(""))
+				if (key.toString().equals("")) {
 					break;
+				}
 
-				System.arraycopy(value.getBytes(), 0,
-						clusterKmerVectors[currCenter], 0,
-						clusterKmerVectors[currCenter].length);
-				
+				clusterCenters[currCenter] = new PresenceVector(value);
 				++currCenter;
 				key.set(0);
 			}
-			
 			reader.close();
 		}
 		
 		if (currCenter == numCenters) {
 			return true;
-		}
-		else {
+			
+		} else {
 			LOG.info("Clusters expected: " + numCenters + ", actual clusters: " +
 					currCenter);
 			return false;
@@ -109,27 +98,12 @@ public class BlastMapper extends Mapper<LongWritable, Text, LongWritable, Text> 
 		Configuration conf = context.getConfiguration();
 		kmerLength = conf.getInt(Blast.KMER_LENGTH, 3);
 		alphabetSize = conf.getInt(Blast.ALPHABET_SIZE, Biology.AMINO_ACIDS.length);
-		numberOfBytes = (int) Math.ceil(Math.pow(alphabetSize, kmerLength) / 8);
-		
-		buildAminoAcidToIndexMap();
 		
 		try {
 			loadClusters(conf);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	/**
-	 * TODO(cmhill): Generates a list of words above a given threshold value.
-	 * 
-	 * @param word
-	 * @return List of words that are above a certain similarity.
-	 */
-	private List<String> getWordsAboveThreshold(String word) {
-		List<String> wordList = new ArrayList<String>();
-		wordList.add(word);
-		return wordList;
 	}
 
 	public void map(LongWritable key, Text value, Context context) 
@@ -149,26 +123,18 @@ public class BlastMapper extends Mapper<LongWritable, Text, LongWritable, Text> 
 		Set<Integer> overlappingClusters = new HashSet<Integer>();
 		
 		// Faster way than this?
-		for (int i = 0; i < numCenters; i++)
+		for (int i = 0; i < numCenters; i++) {
 			clusters.add(i);
+		}
 		
-		for (int i = 0; i <= sequence.length() - kmerLength; i += kmerLength) {
-			// TODO(cmhill): Get all words above the given threshold.
-			List<String> words = getWordsAboveThreshold(sequence.substring(i, i + kmerLength));
+		for (int i = 0; i < sequence.length() - kmerLength; i++) {
 			
-			for (String word : words) {
-				
-				// Cycle through each cluster and if the kmer/word appears in
-				// the cluster, add the cluster to the overlappingClusters set.
-				int wordIndex = getIndexForSequence(word);
-				int byteOffset = wordIndex / 8;
-				int bitOffset = wordIndex % 8;
-				int mask = 1 << bitOffset;
-				
-				//for (int j = 0; j < clusterKmerVectors.length; j++) {
-				for (Integer j : clusters) {
-					// Is this position set in the cluster vector?
-					if ((clusterKmerVectors[j][byteOffset] & mask) != 0) {
+			Set<Integer> kmerHashes = getNeighborKmerHashes(sequence.substring(i, i + kmerLength), 0);
+			for (int kmerHash : kmerHashes) {
+
+				// Check which cluster centers overlap with this k-mer.
+				for (Integer j : clusters) {		
+					if (clusterCenters[j].containsKmer(kmerHash)) {
 						overlappingClusters.add(j);
 					}
 				}
@@ -182,43 +148,20 @@ public class BlastMapper extends Mapper<LongWritable, Text, LongWritable, Text> 
 		for (Integer clusterId : overlappingClusters) {
 			context.write(new LongWritable(clusterId), value);
 		}
-		
-		// Toy example, just send the sequence to the clusterID.
-		/*String[] clusterAndSequence = value.toString().trim().split("\t");
-		
-		context.write(new LongWritable(new Long(clusterAndSequence[0])),
-				new Text(clusterAndSequence[1]));*/
 	}
 	
-	/*
-	 * These functions should be a utility bit vector class 
+	/**
+	 * Returns the hashes of all k-mers which match the given k-mer with the given threshold.
+	 * 
+	 * @param kmer the k-mer to be matched against
+	 * @param threshold the scoring threshold to exceed
+	 * @return the hashes of all matching k-mers
 	 */
-	private Map<Character, Integer> aaToIndex;
-	
-	private void buildAminoAcidToIndexMap() {
-		int index = 0;
-		aaToIndex = new HashMap<Character, Integer>();
-		for (Character aa : Biology.AMINO_ACIDS) {
-			aaToIndex.put(aa, index);
-			index++;
-		}
-	}
-	
-	public int getIndexForSequence(String sequence) {
-		char aminoAcid = 'X';
-		int power = this.kmerLength - 1;
-		int index = 0;
-		
-		for (int i = 0; i < sequence.length(); i++) {
-			aminoAcid = sequence.charAt(i);
-			if (aaToIndex.get(aminoAcid) == null) {
-				System.out.println("aa: " + aminoAcid);
-			} else {
-				index += (aaToIndex.get(aminoAcid) * Math.pow(this.alphabetSize, power));
-				power--;
-			}
-		}
-		
-		return index;
-	}
+	Set<Integer> getNeighborKmerHashes(String kmer, int threshold) {
+		Set<Integer> kmers = new HashSet<Integer>();
+		kmers.add(Biology.getAAKmerHash(kmer));
+
+		// TODO(calbach): Implement threshold matching.
+		return kmers;
+	} 
 }
